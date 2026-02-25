34cb8a4993c1d476748512167df36c9353053590
Armando de Jesus Hidalgo Riquenes <armandohriquenes@gmail.com>
2026-02-25 14:43:02 +0100

    add fotos2.1

--- src/heroes/components/HeroCard.tsx
+++ src/heroes/components/HeroCard.tsx
@@ -13,7 +13,7 @@ export const HeroCard = ({
   first_appearance,
   characters }: any) => {
 
-  const heroImgUrl = `heroes/${id}.jpg`;
+  const heroImgUrl = `${import.meta.env.BASE_URL}heroes/${id}.jpg`;
   return (
     <div className="col animate__animated animate__fadeIn">
       <div className="card">
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp';
 
-
+const basename = import.meta.env.MODE === 'production' ? '/react-heroes-spa' : '';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <HeroesApp />
     </BrowserRouter>
   </StrictMode>,
