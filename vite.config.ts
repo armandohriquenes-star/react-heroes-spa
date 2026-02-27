5a626af245537550a214bce6a06046a630f724c9
Armando de Jesus Hidalgo Riquenes <armandohriquenes@gmail.com>
2026-02-27 15:53:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';  // ✅ Importa el plugin
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '/react-heroes-spa/',
